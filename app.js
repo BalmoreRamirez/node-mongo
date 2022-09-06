260e1af68a27a9eb60e458f19380ad069306c27f
Balmore Ramirez <camaleoncode@gmail.com>
2022-09-05 20:06:01 -0600

    route defaul

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app = express()
 app.use(cors())
 app.use(express.json())
 
+
+app.use('/', (req, res)=>{
+    res.send({data:'Hola mundo'})
+})
+
 app.use('/api', require('./routes/product'))
 app.use('/api', require('./routes/category'))
 
